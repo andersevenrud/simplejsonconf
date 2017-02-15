72ae3bf1b813f19fac6908091ea6f0d22121eb02
Anders Evenrud <andersevenrud@gmail.com>
2017-02-15 22:37:09 +0100

    Merge deep if target type != source type

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function mergeDeep(target, source) {
   if ( isObject(target) && isObject(source) ) {
     for ( const key in source ) {
       if ( isObject(source[key]) ) {
-        if ( !target[key]) {
+        if ( !target[key] || typeof target[key] !== typeof source[key] ) {
           Object.assign(target, {
             [key]: {}
           });
